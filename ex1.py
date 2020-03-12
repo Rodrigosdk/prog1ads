f07ed66aa3a5c941cd78a41ce60b9530fb3309b7
Rodrigosdk <rodrigosilva@ads.fiponline.edu.br>
2020-03-12 10:32:56 -0300

    exception handling

--- ex1.py
+++ ex1.py
@@ -5,13 +5,39 @@ Faça um programa que solicite ao usuário o valor do litro de combustível (ex.
 e quanto em dinheiro ele deseja abastecer (ex. 50,00). Calcule quantos litros de 
 combustível o usuário obterá com esses valores.
 '''
+import os
 
-#Capitura as entradas do usuário
-ask_liter = float(input("Insira o valor do litro da gasolina: "))
-ask_for_value = float(input("Insira o valor de quanto deseja abasticer: "))
+while True:
+    try:
+        # Capitura as entradas do usuário
+        ask_liter = float(input("Insira o valor do litro da gasolina: "))
+        ask_value = float(
+            input("Insira o valor de quanto deseja abasticer: "))
 
-#calcular a quantidades de litros
-cal = ask_for_value/ask_liter
+        # Calcular a quantidades de litros
+        cal = ask_value/ask_liter
 
-#Mostra na tela o resutado
-print(f"Com R${ask_liter} o veículo receber {cal:.2f} litros ")
+        # Mostra na tela o resutado
+        print(f"Com R${ask_value} o veículo receber {cal:.2f} litros ")
+
+        # Canselar o loop
+        break
+
+    # Tratamento de erros com valor de argumento inadequado
+    except (ValueError):
+        # Limpar a tela
+        os.system("clear")
+
+        print("="*80)
+        # Mostra mensagem de erro em cor vermelha e centralizado
+        print("{}Erro: só é permitido números e ponto{}".format(
+            '\033[31m', '\033[m').center(80))
+        print("="*80)
+
+    # Tratamento de erro de interupção
+    except(KeyboardInterrupt, EOFError):
+        # Limpar a telar
+        os.system("clear") or None
+
+        # Canselar o loop
+        break
