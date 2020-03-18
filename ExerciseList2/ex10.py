41b14b44833ea9a7946efd41af08c4d05e11acd2
Rodrigosdk <rodrigosilva@ads.fiponline.edu.br>
2020-03-18 18:31:10 -0300

    lista de exercicios 2

---
+++ ExerciseList2/ex10.py
@@ -0,0 +1,20 @@
+'''
+10 - Faça um Programa que pergunte em que turno você estuda.
+Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
+Imprima a mensagem "Bom Dia!", "Boa Tarde!"
+ ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+'''
+
+# variáveis
+turno = str(input('Em que turno você estuda, digite "M" matutino ou "V" Vespertino ou "N" Noturno? ')).strip().upper()
+
+# informando o turo que estuda
+
+if turno == 'M':
+    print('Bom Dia!')
+elif turno == 'V':
+    print('Boa Tarde!')
+elif turno == 'N':
+    print('Boa Noite!')
+else:
+    print('Valor Inválido!')
---
+++ ExerciseList2/ex4.py
@@ -0,0 +1,13 @@
+'''
+4 - Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+'''
+
+#variáveis
+letra = str(input('Informe uma letra qualquer: ')).strip().upper()
+
+#comparando se a letra é uma vogal ou uma consoante
+if letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
+    print(f'A letra digitada foi "{letra}" e ela é uma vogal.')
+else:
+    print(f'A letra digitada foi "{letra}" e ela é uma consoante.')
+
