e6f54b7e2323e3db68092433d344cbc51e870093
Rodrigosdk <rodrigosilva@ads.fiponline.edu.br>
2020-05-06 18:16:36 -0300

    criado a função counter

--- ExerciseList5/ex6.py
+++ ExerciseList5/ex6.py
@@ -4,18 +4,35 @@ um abaixo do outro.Depois modique o programa para que ele
 mostre os números um ao lado do outro.
 '''
 
-#Cria contador
-cont = 0
 
-# Valida as entradas
-while cont != 20:
-    cont += 1   
-    print(cont)
+def counter(init, end, type_line=None):
+    '''
+    init: valor inicial;
     
-# zera o contador 
-cont = 0
+    end: valor final;
 
-# Valida as entradas
-while cont != 20:
-    cont += 1
-    print(cont, end=' ')
+    type_line = como a mensegem deve se apresentar. ex:
+    
+    counter(1, 21) ==> printa o valor de 1 até 20;
+    
+    counter(1, 21, ",") ==> printa os valores em uma unica linha segido pelo 
+    argumento recebido por type_line: 1,2,3,4...20
+    
+    '''
+    try:
+        # Valida as entradas
+        while init != end:
+            
+            #verifica como deve ser inpresa a mensagem
+            if type_line == None:
+                print(init)
+
+            else: 
+                print(init, end=type_line)
+            
+            init += 1
+    except TypeError:
+        print('O valor informado deve ser inteiro')
+
+counter(1, 21)
+counter(1, 21)
